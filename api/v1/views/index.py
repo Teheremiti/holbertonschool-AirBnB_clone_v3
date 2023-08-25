aa334722efe1360cf23c644e0c0000d244e9dfcf
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-25 17:09:14 +0200

    Fix route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,7 +18,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route("/api/v1/stats", strict_slashes=False)
 def stats():
     """Returns the number of objects by type"""
     classes = {
